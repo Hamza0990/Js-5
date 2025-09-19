28fa2020c934fbf0fd369c98b4266774463cfed7
Hamza0990 <hamzaquluyev09@gmail.com>
2025-09-19 13:43:44 +0400

    Js-5

---
+++ app.js
@@ -0,0 +1,21 @@
+let a = 2468;
+console.log(a);
+let b = 2 + 4 + 6 + 8;
+console.log(b);
+console.log(null);
+// 1- ci tapsiriq bitdi
+
+let fulName = prompt("Write your name and surname");
+console.log("Salam" +  fulName.split(" ")[1] + "!");
+console.log(null);
+
+// 2- ci tapsiriq bitdi 
+
+let age = prompt ("Are you 18 years old ?");
+if (age>18) {
+    console.log("Access Grantend")
+}
+if (age<18) {
+    console.log("Access denied")
+}
+// 3-cu tapsiriq bitdi
